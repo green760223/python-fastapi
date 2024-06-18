52f5e456cb18cc9fda3c3cc3bf0abba2b4c3577c
Lawrence Yi-Hsuan Chuang <green760223@gmail.com>
2024-06-18 01:48:53 -0700

    Configure logging method to save into a file to store log information

--- storeapi/logging_conf.py
+++ storeapi/logging_conf.py
@@ -13,21 +13,38 @@ def configure_logging() -> None:
                     "class": "logging.Formatter",
                     "datefmt": "%Y-%m-%dT%H:%M:%S",
                     "format": "%(name)s:%(lineno)d - %(message)s",
-                }
+                },
+                "file": {
+                    "class": "logging.Formatter",
+                    "datefmt": "%Y-%m-%dT%H:%M:%S",
+                    "format": "%(asctime)s.%(msecs)03dZ | %(levelname)-8s | :%(name)s:%(lineno)d - %(message)s",
+                },
             },
             "handlers": {
                 "default": {
                     "class": "rich.logging.RichHandler",
                     "level": "DEBUG",
                     "formatter": "console",
-                }
+                },
+                "rotating_file": {
+                    "class": "logging.handlers.RotatingFileHandler",
+                    "level": "DEBUG",
+                    "formatter": "file",
+                    "filename": "storeapi.log",
+                    "maxBytes": 1024 * 1024,  # 1 MB per file
+                    "backupCount": 5,
+                    "encoding": "utf8",
+                },
             },
             "loggers": {
+                "uvicorn": {"handlers": ["default", "rotating_file"], "level": "INFO"},
                 "storeapi": {
-                    "handlers": ["default"],
+                    "handlers": ["default", "rotating_file"],
                     "level": "DEBUG" if isinstance(config, DevConfig) else "INFO",
                     "propagate": False,
-                }
+                },
+                "database": {"handlers": ["default"], "level": "WARNING"},
+                "aiosqlite": {"handlers": ["default"], "level": "WARNING"},
             },
         }
     )
