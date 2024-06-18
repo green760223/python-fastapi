6db29c937f0164f6c81c34ecbab395d3e8437720
Lawrence Yi-Hsuan Chuang <green760223@gmail.com>
2024-06-18 01:13:50 -0700

    Add logging methed to log API information

--- storeapi/main.py
+++ storeapi/main.py
@@ -1,14 +1,21 @@
+import logging
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 
+
 from storeapi.database import database
+from storeapi.logging_conf import configure_logging
 from storeapi.routers.post import router as post_router
+
+logger = logging.getLogger(__name__)
 
 
 # The lifespan context manager is used to connect and disconnect from the database.
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    configure_logging()
+    logger.info("Hello world")
     await database.connect()
     yield
     await database.disconnect()
