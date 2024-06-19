67af3a79a751dbbdb9a71762b70b09806bc42cc9
Lawrence Yi-Hsuan Chuang <green760223@gmail.com>
2024-06-19 00:52:54 -0700

    Add correlation id to identify the logs from same request

--- storeapi/main.py
+++ storeapi/main.py
@@ -1,8 +1,9 @@
 import logging
 from contextlib import asynccontextmanager
 
-from fastapi import FastAPI
-
+from asgi_correlation_id import CorrelationIdMiddleware
+from fastapi import FastAPI, HTTPException
+from fastapi.exception_handlers import http_exception_handler
 
 from storeapi.database import database
 from storeapi.logging_conf import configure_logging
@@ -23,6 +24,14 @@ async def lifespan(app: FastAPI):
 
 #
 app = FastAPI(lifespan=lifespan)
+app.add_middleware(CorrelationIdMiddleware)
 
 # Include the post router in the app
 app.include_router(post_router)
+
+
+# The exception handler logs the exception and then calls the default exception handler.
+@app.exception_handler(HTTPException)
+async def http_exception_handler_logging(request, exc):
+    logger.error(f"HTTPException: {exc.status_code} {exc.detail}")
+    return await http_exception_handler(request, exc)
--- storeapi/routers/post.py
+++ storeapi/routers/post.py
@@ -48,7 +48,6 @@ async def get_all_posts():
 async def create_comment(comment: CommentIn):
     post = await find_post(comment.post_id)
     if not post:
-        logger.error(f"Post with id {comment.post_id} not found")
         raise HTTPException(status_code=404, detail="Post not found")
 
     data = comment.model_dump()
@@ -75,7 +74,6 @@ async def get_post_with_comments(post_id: int):
 
     post = await find_post(post_id)
     if not post:
-        logger.error(f"Post with id {post_id} not found")
         raise HTTPException(status_code=404, detail="Post not found")
 
     return {
