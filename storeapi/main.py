ddfd55b430872f4f923d3a3b1b3f20bf51094ead
Lawrence Yi-Hsuan Chuang <green760223@gmail.com>
2024-07-13 02:40:02 -0700

    Update sentry config setting

--- storeapi/main.py
+++ storeapi/main.py
@@ -1,15 +1,29 @@
 import logging
 from contextlib import asynccontextmanager
 
+import sentry_sdk
 from asgi_correlation_id import CorrelationIdMiddleware
 from fastapi import FastAPI, HTTPException
 from fastapi.exception_handlers import http_exception_handler
 
+from storeapi.config import config
 from storeapi.database import database
 from storeapi.logging_conf import configure_logging
 from storeapi.routers.post import router as post_router
 from storeapi.routers.upload import router as upload_router
 from storeapi.routers.user import router as user_router
+
+# Sentry SDK is used to capture errors and performance data.
+sentry_sdk.init(
+    dsn=config.SENTRY_DSN,
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    traces_sample_rate=1.0,
+    # Set profiles_sample_rate to 1.0 to profile 100%
+    # of sampled transactions.
+    # We recommend adjusting this value in production.
+    profiles_sample_rate=1.0,
+)
 
 logger = logging.getLogger(__name__)
 
@@ -32,6 +46,13 @@ app.add_middleware(CorrelationIdMiddleware)
 app.include_router(post_router)
 app.include_router(user_router)
 app.include_router(upload_router)
+
+
+# This route triggers a division by zero error to test Sentry.
+# @app.get("/sentry-debug")
+# async def trigger_error():
+#     division_by_zero = 1 / 0
+#     return division_by_zero
 
 
 # The exception handler logs the exception and then calls the default exception handler.
